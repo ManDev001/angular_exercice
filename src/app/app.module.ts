92597a9813762bb2beec291b50cd48aee4592a57
nasrii001 <user-email>
2019-03-20 20:57:27 +0100

    [main module] added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './header/header.module';
+import { MainModule } from './main/main.module';
 
 @NgModule({
    declarations: [
@@ -13,6 +14,7 @@ import { HeaderModule } from './header/header.module';
       BrowserModule,
       AppRoutingModule,
       HeaderModule,
+      MainModule
    ],
    providers: [],
    bootstrap: [
